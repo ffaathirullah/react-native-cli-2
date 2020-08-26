c25d4a0f5deab098a0a72faec670591f697cb7cc
Fachrul <ffaathirullah1@gmail.com>
2020-08-26 10:42:03 +0700

    basic layouting, flexbox

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {View, ScrollView} from 'react-native';
 import SampleComponent from './SampleComponent';
 import StylingReactComponent from './StylingComponent';
-
+import MateriFlexbox from './MateriFlexbox';
 const App = () => (
   <View>
     <ScrollView>
       <SampleComponent />
       <StylingReactComponent />
+      <MateriFlexbox />
     </ScrollView>
   </View>
 );
---
+++ MateriFlexbox.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {Text, View, Image} from 'react-native';
+
+class MateriFlexbox extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Materi Flexbox : </Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: 'grey',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <View style={{backgroundColor: 'blue', width: 50, height: 50}} />
+          <View style={{backgroundColor: 'yellow', width: 50, height: 60}} />
+          <View style={{backgroundColor: 'orange', width: 50, height: 70}} />
+          <View style={{backgroundColor: 'red', width: 50, height: 80}} />
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+          }}>
+          <Text>Home</Text>
+          <Text>Body</Text>
+          <Text>Center</Text>
+        </View>
+        <View style={{flexDirection: 'row', margin: 40}}>
+          <Image
+            source={{uri: 'http://placeimg.com/100/100/animals'}}
+            style={{width: 70, height: 70, borderRadius: 50}}
+          />
+          <View>
+            <Text> Facheul Faathirullah </Text>
+            <Text> Coba React Native</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default MateriFlexbox;
